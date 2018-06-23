d7c3727136e19ce2217dbbd61f69ee3f2f17587b
Kiran Ayyagari <kayyagari@keydap.com>
2018-06-23 17:44:42 +0530

    append colon when prefixed with extension schema URI

--- scim.pointer.ts
+++ scim.pointer.ts
@@ -9,9 +9,15 @@ export class ScimPointer extends Pointer {
       let l = this.tokens.length;
       if(l > 1) {
           let str = this.tokens[0];
+          var colonPos = str.lastIndexOf(':')
           for(let i =1; i < this.tokens.length; i++) {
               let nextToken: string = this.tokens[i];
-              if(nextToken[0] != '[') {
+              // special case of extension URI in the path
+              // only the first attribute is prepended with ':'
+              if(colonPos > 0 && i==1) {
+                str += ':';
+              }
+              else if(nextToken[0] != '[') {
                   str += '.';
               }
 
